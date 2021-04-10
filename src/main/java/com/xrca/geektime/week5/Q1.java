05e676cb6b73096d282a9a57ac6ebae17655ed91
xrca <fishsaury@163.com>
2021-04-10 15:48:59 +0800

    算法打卡第5周

---
+++ src/main/java/com/xrca/geektime/week5/Q1.java
@@ -0,0 +1,39 @@
+package com.xrca.geektime.week5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 汉诺塔问题 https://leetcode-cn.com/problems/hanota-lcci/
+public class Q1 {
+
+    public static void main(String[] args) {
+        List<Integer> A = new ArrayList<>();
+        A.add(3);
+        A.add(2);
+        A.add(1);
+        List<Integer> B = new ArrayList<>();
+        List<Integer> C = new ArrayList<>();
+        Q1 q1 = new Q1();
+        q1.hanota(A, B, C);
+    }
+
+    public void hanota(List<Integer> A, List<Integer> B, List<Integer> C) {
+        move(A.size(), A, B, C);
+        System.out.println();
+    }
+
+    public void move(int n, List<Integer> A, List<Integer> B, List<Integer> C) {
+        // 如果只有一个盘子，则直接移动到C
+        if (n == 1) {
+            C.add(A.remove(A.size() - 1));
+            return;
+        } else {
+            // 将A上 1 至 n - 1的盘子移动到B，以C为辅助
+            move(n - 1, A, C, B);
+            //将A上的n号盘子移动到C
+            C.add(A.remove(A.size() - 1));
+            // 将B上1 - n的盘子移到C，以A为辅助
+            move(n - 1, B, A, C);
+        }
+    }
+}
---
+++ src/main/java/com/xrca/geektime/week5/Q2.java
@@ -0,0 +1,114 @@
+package com.xrca.geektime.week5;
+
+import javax.management.StandardEmitterMBean;
+import java.util.*;
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        Q2 q2 = new Q2();
+        ListNode node = new ListNode(4);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(1);
+        ListNode node4 = new ListNode(3);
+        node.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        ListNode rnode = new ListNode(4);
+        ListNode rnode2 = new ListNode(2);
+        ListNode rnode3 = new ListNode(1);
+        ListNode rnode4 = new ListNode(3);
+        rnode.next = rnode2;
+        rnode2.next = rnode3;
+        rnode3.next = rnode4;
+        ListNode result = q2.sortList2(node);
+        System.out.println(result);
+    }
+
+    public ListNode sortList(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        Collections.sort(list, Comparator.comparingInt(o -> o.val));
+        ListNode resultNode = null, lastNode = null;
+        for (ListNode node : list) {
+            node.next = null;
+            if (lastNode == null) {
+                resultNode = node;
+            } else {
+                lastNode.next = node;
+            }
+            lastNode = node;
+        }
+        return resultNode;
+    }
+
+    public ListNode sortList2(ListNode head) {
+        return mergeSort(head);
+    }
+
+    // 归并排序
+    public ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head, fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode right = mergeSort(slow.next);
+        // 断开连接
+        slow.next = null;
+        ListNode left = mergeSort(head);
+        return mergeList(left, right);
+    }
+
+    // 合并list
+    public ListNode mergeList(ListNode left, ListNode right) {
+        ListNode tmpHead = new ListNode(), currentHead = tmpHead;
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                currentHead.next = left;
+                currentHead = left;
+                left = left.next;
+            } else {
+                currentHead.next = right;
+                currentHead = right;
+                right = right.next;
+            }
+        }
+        if (left != null) {
+            currentHead.next = left;
+        }
+        if (right != null) {
+            currentHead.next = right;
+        }
+        return tmpHead.next;
+    }
+
+    // 节点
+    public static class ListNode {
+        int val;
+
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return this.val + (next == null ? "" : " -> " + next.toString());
+        }
+    }
+}
