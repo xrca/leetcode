5a0d24eedb069a639b451db331e373ab31a4e62c
xrca <fishsaury@163.com>
2021-04-28 23:24:56 +0800

    每日一题：2021.04.28-平方数之和

---
+++ src/main/java/com/xrca/daily/Day20210428.java
@@ -0,0 +1,30 @@
+package com.xrca.daily;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//  平方数之和 https://leetcode-cn.com/problems/sum-of-square-numbers/
+public class Day20210428 {
+    public static void main(String[] args) {
+        Day20210428 day = new Day20210428();
+        System.out.println(day.judgeSquareSum(16));
+    }
+
+    //
+    public boolean judgeSquareSum(int c) {
+        int low = 0;
+        int high = ((int)Math.sqrt(c + 0.0)) + 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = low; i < high; i++) {
+            if (map.containsKey(c - i * i) || i * i * 2 == c) {
+                return true;
+            }
+            map.put(i * i, i);
+        }
+        return false;
+    }
+
+}
+
+
